5f5beb9e2a86aa13edef46358cb96db0cedca917
joakim <kiungi@mail.com>
2022-09-13 20:32:24 +0300

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * main - entry point
  *
- * Description: Prints 0-putchar with _putchar
+ * Description: Prints Holberton with _putchar
  *
  * Return: always 0
  */
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i = 0;
 	char c;
-	char s[] = "0-putchar\n";
+	char s[] = "Holberton\n";
 
 	while (i <= 9)
 	{
