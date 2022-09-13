bd592443d1a954651373afc1413a2d276b7e30c2
joakim <kiungi@mail.com>
2022-09-13 20:14:31 +0300

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "0-putchar.h"
+
+/**
+ * main - entry point
+ *
+ * Description: Prints 0-putchar with _putchar
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char c;
+	char s[] = "0-putchar\n";
+
+	while (i <= 9)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
